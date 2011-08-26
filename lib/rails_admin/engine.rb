7b748698cc356afef10d4aff82b4a66e9e402dd0
Michael Potter <mike@disrupto.com>
2011-08-26 15:22:06 -0400

    Remove custom asset pipeline changes (moved to app).

--- lib/rails_admin/engine.rb
+++ lib/rails_admin/engine.rb
@@ -14,14 +14,5 @@ module RailsAdmin
         RailsAdmin.reset if !app.config.cache_classes && RailsAdmin.config.reload_between_requests
       end
     end
-    
-    initializer "add assets to precompile pipeline" do |app|
-      # All JS
-      app.config.assets.precompile += Dir.glob(File.expand_path("../../../app/assets/javascripts/rails_admin/rails_admin.js", __FILE__))
-      # All CSS images
-      app.config.assets.precompile += Dir.glob(File.expand_path("../../../app/assets/stylesheets/rails_admin/rails_admin.css", __FILE__))
-      # All images
-      app.config.assets.precompile += Dir.glob(File.expand_path("../../../app/assets/images/rails_admin/**/*", __FILE__))
-    end
   end
 end
